26f15320ce0de304a7eeba2f8411a42a7d32af29
PiyushKalbande <piyushkalbande111@gmail.com>
2025-12-16 11:43:55 +0530

    adding some lines in python and html header for icons

--- app.py
+++ app.py
@@ -13,7 +13,22 @@ load_dotenv()
 app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY')
 
-gc = gspread.service_account(filename="service.json")
+
+
+service_info = {
+    "type": "service_account",
+    "project_id": os.getenv("GOOGLE_PROJECT_ID"),
+    "private_key_id": os.getenv("GOOGLE_PRIVATE_KEY_ID"),
+    "private_key": os.getenv("GOOGLE_PRIVATE_KEY").replace("\\n", "\n"),
+    "client_email": os.getenv("GOOGLE_CLIENT_EMAIL"),
+    "client_id": os.getenv("GOOGLE_CLIENT_ID"),
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": f"https://www.googleapis.com/robot/v1/metadata/x509/{os.getenv('GOOGLE_CLIENT_EMAIL')}"
+}
+
+gc = gspread.service_account_from_dict(service_info)
 G_KEY = os.getenv('GOOGLE_SHEET_KEY') 
 sh = gc.open_by_key(G_KEY)
 worksheet = sh.sheet1
